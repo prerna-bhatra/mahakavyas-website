c6695ac5f1fdd3caa111a3c01566c9a1f919bab9
p.bhatra@thefifthelement.co <bhatraprerna061@gmail.com>
2023-09-07 12:36:37 +0530

    chapters

--- src/components/Chapter.js
+++ src/components/Chapter.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+
+
 import './chapter.css'
 
 const Chapter = () => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+
     const { id } = useParams();
-    console.log({ id });
+    // console.log({ id });
 
     useEffect(() => {
+
         fetchData(id);
     }, [id]);
 
@@ -35,15 +39,40 @@ const Chapter = () => {
 
     console.log({ data });
 
+    const handleListen = (chapterContent) => {
+       
+
+    }
+
+    const handlePause = (id) => {
+
+    }
+
+    const handleStop = (id) => {
+        
+
+    }
+
+    const handleResume = (id) => {
+
+    }
+
     return (
         <div className="chapter">
             {isLoading ? (
                 <p>Loading data...</p>
             ) : (
                 <div className="chapter-container">
-                    {data.map((item) => (
+                    {data.map((item, index) => (
                         <div className="chapter-card" key={item.puranId}>
-                            <h2>{item.title}</h2>
+                            <h2>अध्याय {index + 1} : {item.title}</h2>
+                            <div className="button-container">
+                                <button onClick={() => handleListen(item.content)}>Listen</button>
+                                <button onClick={() => handlePause(item.content)}>Pause</button>
+                                <button onClick={() => handleResume(item.content)}>Resume</button>
+                                <button onClick={() => handleStop(item.content)}>Stop</button>
+
+                            </div>
                             <p>{item.content}</p>
                         </div>
                     ))}
